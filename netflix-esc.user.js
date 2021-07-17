71dab63812e419e941eb1df8d6b173edbe9ca1a1
Teddy Wing <git@teddywing.com>
2021-07-17 14:40:25 +0200

    Increase version v0.0.3 -> v0.0.4

--- netflix-esc.user.js
+++ netflix-esc.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name Netflix Esc
 // @description Add keyboard shortcuts to Netflix
-// @version 0.0.3
+// @version 0.0.4
 // @namespace com.teddywing
 // @match https://www.netflix.com/*
 // ==/UserScript==
